0103b596928f518bfb732cbec9573cdfa18676ba
BobbyL2k <bobby_nh@hotmail.com>
2017-09-03 21:44:14 +0700

    Web-Crawler:  Client:   Add tabs remote execution with persistent stroage

--- web-crawler/client/background.js
+++ web-crawler/client/background.js
@@ -12,6 +12,9 @@ const enm = {
 
 // Functions definition
 function timedCallback(wait_ms, callback) {
+    // create a new proxy callback function that will execute the
+    // provided callback if a certain amount of time have passed and
+    // the proxy was not called
     var tooLate = false;
     var timer = setTimeout(() => {
         tooLate = true;
@@ -27,7 +30,7 @@ function timedCallback(wait_ms, callback) {
 }
 
 const tab_action = {
-    check_ok: function (tabId, callback) {
+    checkOk: function (tabId, callback) {
         var message = {
             type: 'ok'
         };
@@ -45,71 +48,99 @@ const state_action = {
     removeTab: function (tabId) {
         if (global.tabs[tabId] !== undefined) {
             delete global.tabs[tabId];
-            extension_action.update_tabs_count();
+            extension_action.updateTabsCount();
         } else {
             console.log('error removeTab: tab with tabId', tabId, 'not found');
         }
     },
     addTab: function (tabObject) {
-        if(global.tabs[tabObject.id] === undefined){
+        if (global.tabs[tabObject.id] === undefined) {
             global.tabs[tabObject.id] = tabObject;
-            extension_action.update_tabs_count();
+            extension_action.updateTabsCount();
 
             if (state_info.getTabCount() == 1) {
-                extension_action.clean_dead_tabs();
+                extension_action.cleanDeadTabs();
             }
         } else {
             console.log('error addTab: tab with tabId', tabId, 'already exist');
+        }
+    },
+    setInjectionToTab: function (tabId, code) {
+        if (global.tabs[tabId] !== undefined) {
+            global.tabs[tabId].code = code;
+        } else {
+            console.log('error setInjectionToTab: tab with tabId', tabId, 'not found');
         }
     }
 }
 
 const state_info = {
-    getTabCount: function(){
+    getTabCount: function () {
         return Object.keys(global.tabs).length;
     }
 }
 
 const extension_action = {
-    update_tabs_count: function () {
+    updateTabsCount: function () {
         chrome.browserAction.setBadgeText({ text: state_info.getTabCount().toString() });
     },
-    start_up: function () {
+    startUp: function () {
         console.log("bg page started");
-        extension_action.update_tabs_count();
+        extension_action.updateTabsCount();
     },
-    clean_dead_tabs: function () {
+    cleanDeadTabs: function () {
         console.log('checking dead', global.tabs);
-        for(var tabId in global.tabs){
+        for (var tabId in global.tabs) {
             tab = global.tabs[tabId];
             if (tab.status === enm.status.ALIVE) {
-                tab_action.check_ok(tab.id, (ok) => {
+                tab_action.checkOk(tab.id, (ok) => {
                     if (!ok) {
                         console.log('tab', tab.id, 'is DEAD, killing tab.');
                         tab.status = enm.status.DEAD;
-                        chrome.tabs.remove(tabId);
-                        state_action.removeTab(tabId);
                     } else {
                         console.log('tab', tab.id, 'is ok.');
                     }
                 });
             }
+            // Note: can NOT use else if due to the ALIVE condition marking the tab as DEAD
+            if (tab.status === enm.status.DEAD) {
+                // Note: chrome.tabs.remove requires int as tab's ID
+                chrome.tabs.remove(tab.id);
+                state_action.removeTab(tabId);
+            }
+            if (tab.status === enm.status.LOADING) {
+                console.log('skipping check tab', tabId, 'with status', tab.status);
+            }
         }
         if (state_info.getTabCount() > 0)
-            setTimeout(extension_action.clean_dead_tabs, 5000);
+            setTimeout(extension_action.cleanDeadTabs, 5000);
+    },
+    injectCodeToTab: function (tabId) {
+        if (global.tabs[tabId] !== undefined) {
+            if (global.tabs[tabId].code !== undefined) {
+                console.log('injecting code to tab', tabId);
+                chrome.tabs.executeScript(tabId, {
+                    code: global.tabs[tabId].code
+                });
+            }else{
+                console.log('warning injectCodeToTab: no code to inject to tab', tabId)
+            }
+        } else {
+            console.log('error injectCodeToTab: tab with tabId', tabId, 'not found');
+        }
     }
 }
 
-function tabs_do(func, callback){
-    global.tabs.forEach((tab)=>{
-        chrome.tabs.executeScript(tab.id, {
-            code: func
-        }, callback);
-    });
-}
+// function tabsDo(func, callback) {
+//     global.tabs.forEach((tab) => {
+//         chrome.tabs.executeScript(tab.id, {
+//             code: func
+//         }, callback);
+//     });
+// }
 
 // Start Extension
-extension_action.start_up();
+extension_action.startUp();
 
 // Setup Events Handling
 chrome.browserAction.onClicked.addListener(function (/*tab*/) {
@@ -120,7 +151,9 @@ chrome.browserAction.onClicked.addListener(function (/*tab*/) {
 
         var tabObject = {
             id: tab.id,
-            status: enm.status.LOADING
+            status: enm.status.LOADING,
+            storage: { test: 2 },
+            code: undefined
         };
 
         state_action.addTab(tabObject);
@@ -130,21 +163,59 @@ chrome.browserAction.onClicked.addListener(function (/*tab*/) {
 
 chrome.tabs.onRemoved.addListener(function (tabId, removeInfo) {
     if (global.tabs[tabId] !== undefined) {
-        console.log('managed tab closed');
+        console.log('managed tab', tabId, 'closed');
         state_action.removeTab(tabId);
     }
 });
 
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab){
-    if(global.tabs[tabId] !== undefined){
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    if (global.tabs[tabId] !== undefined) {
         console.log('chrome.tabs.onUpdated on managed tab', tabId, changeInfo, tab);
-        if(changeInfo.status == 'complete'){
+        if (changeInfo.status == 'complete') {
             chrome.tabs.executeScript(tabId, {
                 file: 'page.js'
-            }, function(){
-                console.log('Injected extension page script into tab', tabId);
+            }, function () {
+                console.log('injected extension page script into tab', tabId);
                 global.tabs[tabId].status = enm.status.ALIVE;
+
+                console.log('sending tab storage', global.tabs[tabId].storage, global.tabs[tabId], 'into tab', tabId); /// TO REMOVE
+                chrome.tabs.sendMessage(tabId, {
+                    type: 'storage',
+                    storage: global.tabs[tabId].storage
+                }, {}, (newStorage) => {
+                    console.log('recived new storage', newStorage, 'from tab', tabId);
+                    if(newStorage === undefined){
+                        console.log('chrome.runtime.lastError', chrome.runtime.lastError);
+                    }else{
+                        global.tabs[tabId].storage = newStorage;
+                    }
+                    // global.tabs[tabId].status = enm.status.DEAD;
+                });
+
+                state_action.setInjectionToTab(tabId,
+                    `
+                    console.log('eiei');
+                    const func = ${pageInject.toString()};
+                    set_func(func);
+                    `
+                )
+                extension_action.injectCodeToTab(tabId);
             });
         }
     }
 });
+
+function pageInject(storage, callback) {
+    console.log('storage', storage);
+    const urlRegex = /https\:\/\/pantip.com/g;
+    if (urlRegex.test(document.location) == false) {
+        setTimeout(() => {
+            console.log('redirecting');
+            document.location = 'https://pantip.com/';
+        }, 1000);
+    }
+    else {
+        storage.test++;
+        callback(null, storage);
+    }
+}
--- web-crawler/client/page.js
+++ web-crawler/client/page.js
@@ -1,5 +1,9 @@
 console.log('hello from page');
-// window.location = 'https://pantip.com';
+
+var tabStorage = undefined;
+var tabFunc = undefined;
+var tabSendResponse = undefined;
+
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse){
     console.log('message recevied', message);
     if(message.type == 'ok'){
@@ -7,4 +11,32 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse){
             value:'ok'
         });
     }
+    if(message.type == 'storage'){
+        console.log('recived tab storage');
+        tabStorage = message.storage;
+        tabSendResponse = sendResponse;
+        if(run_func() == false){
+            return true;
+        }
+    }
 });
+
+function set_func(func){
+    tabFunc = func;
+    run_func();
+}
+
+function run_func(sendResponse){
+    console.log('run_func: tabFunc && tabStorage && tabSendResponse', tabFunc !== undefined, tabStorage !== undefined, tabSendResponse !== undefined);
+    if(tabFunc && tabStorage && tabSendResponse){
+        console.log('executing injection');
+        tabFunc(tabStorage, (err, newStorage)=>{
+            if(err) return console.log(err);
+            console.log('sending new storage', newStorage);
+            tabSendResponse(newStorage)
+        });
+        return true;
+    }else{
+        return false;
+    }
+}
